f86be3f1f5afa0436b2d5037308f689f1b796fce
mquang279 <quangm279@gmail.com>
2026-05-14 21:10:38 +0700

    feat: add notification screen when clicking notification icon

---
+++ app/src/main/java/com/mobile/travelhub/usecase/GetNotificationsUseCase.kt
@@ -0,0 +1,16 @@
+package com.mobile.travelhub.usecase
+
+import com.mobile.travelhub.data.NotificationRepository
+import com.mobile.travelhub.data.model.NotificationResponse
+import javax.inject.Inject
+
+class GetNotificationsUseCase @Inject constructor(
+    private val notificationRepository: NotificationRepository
+) {
+    suspend operator fun invoke(pageNumber: Int = 0, pageSize: Int = 10): Result<List<NotificationResponse>> {
+        return notificationRepository.getUnreadNotifications(
+            pageNumber = pageNumber,
+            pageSize = pageSize
+        )
+    }
+}
